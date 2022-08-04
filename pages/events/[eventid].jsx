1582da208e415adb56fbfbb1b4ef9610a46d044c
Paul Park <paulpark330@hotmail.com>
2022-08-04 09:15:09 -0700

    added event details page (dynamic)

--- pages/events/[eventid].jsx
+++ pages/events/[eventid].jsx
@@ -1,7 +1,36 @@
+import { getEventById } from "../../dummy-data";
+import { useRouter } from "next/router";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 const EventDetailPage = () => {
+  const router = useRouter();
+
+  const { eventId } = router.query;
+
+  console.log(eventId);
+
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
     <div>
-      <h1>Event Detail Page</h1>
+      <>
+        <EventSummary title={event.title} />
+        <EventLogistics
+          date={event.date}
+          address={event.location}
+          image={event.image}
+          imageAlt={event.title}
+        />
+        <EventContent>
+          <p>{event.description}</p>
+        </EventContent>
+      </>
     </div>
   );
 };
