c7a9ed8c645f8b6e156f41aff3023244e63ac091
Paul Park <paulpark330@hotmail.com>
2022-08-04 08:49:49 -0700

    styled components with css module

--- components/events/event-list.jsx
+++ components/events/event-list.jsx
@@ -1,8 +1,9 @@
 import EventItem from "./event-item";
+import styles from "./event-list.module.css";
 
 const EventList = ({ items }) => {
   return (
-    <ul>
+    <ul className={styles.list}>
       {items.map((event) => (
         <EventItem key={event.id} {...event} />
       ))}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
 import { getFeaturedEvents } from "../dummy-data";
 import EventList from "../components/events/event-list";
 
+
+
 const HomePage = () => {
   const featuredEvents = getFeaturedEvents();
 
