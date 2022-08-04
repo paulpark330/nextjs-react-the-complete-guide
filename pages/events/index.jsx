9d0f71c209dbb5ade05bfff022ce8873663984d9
Paul Park <paulpark330@hotmail.com>
2022-08-04 09:27:00 -0700

    worked on all events page

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,7 +1,12 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+
 const AllEvents = () => {
+  const events = getAllEvents();
+
   return (
     <div>
-      <h1>All Events</h1>
+      <EventList items={events} />
     </div>
   );
 };
