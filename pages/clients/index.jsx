86b8455790bc400a38e9c92f96f4f459cb0793f8
Paul Park <paulpark330@hotmail.com>
2022-08-03 23:16:20 -0700

    different way of setting link herf

--- pages/clients/index.jsx
+++ pages/clients/index.jsx
@@ -19,7 +19,10 @@ const ClientsPage = () => {
       <ul>
         {clients.map((client) => (
           <li key={client.id}>
-            <Link href={`/clients/${client.id}`}>
+            <Link href={{
+              pathname: "/clients/[id]",
+              query: { id: client.id }
+            }}>
               <a>{client.name}</a>
             </Link>
           </li>
