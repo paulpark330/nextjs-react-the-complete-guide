8884910b476af2f9929a632c62034f64c002e093
Paul Park <paulpark330@hotmail.com>
2022-08-04 09:39:59 -0700

    navigating to filtered events page

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,13 +1,21 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
+import { useRouter } from "next/router";
 
 const AllEvents = () => {
   const events = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
