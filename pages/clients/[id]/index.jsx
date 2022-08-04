262c9c4e31d9231ecfd5a226f9328b6e97d05199
Paul Park <paulpark330@hotmail.com>
2022-08-03 23:21:49 -0700

    navigating programmatically

--- pages/clients/[id]/index.jsx
+++ pages/clients/[id]/index.jsx
@@ -5,9 +5,20 @@ const ClientProjectsPage = () => {
 
   console.log(router.query);
 
+  const loadProjectHandler = () => {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: {
+        id: "max",
+        clientprojectid: "project1",
+      },
+    });
+  };
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
