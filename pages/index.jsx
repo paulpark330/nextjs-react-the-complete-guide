55e5084ee957d41bd3032ad89ba90858cd0671b0
Paul Park <paulpark330@hotmail.com>
2022-08-03 23:10:02 -0700

    navigating with link component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,23 @@
-import styles from "../styles/Home.module.css";
-
-const Home = () => {
+import Link from "next/link";
+const HomePage = () => {
   return (
-    <div className={styles.container}>
-      <h1>Hello Next World!!</h1>
+    <div>
+      <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">
+            Portfolio
+          </Link>
+        </li>
+        <li>
+          <Link href="/clients">
+            Clients
+          </Link>
+        </li>
+
+      </ul>
     </div>
   );
 }
 
-export default Home;
+export default HomePage;
