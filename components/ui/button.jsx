0dffab54dcf883f7cea4437752a81de6238e5d9b
Paul Park <paulpark330@hotmail.com>
2022-08-04 09:33:57 -0700

    added a filter form for filtering events

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -1,11 +1,19 @@
 import Link from "next/link";
 import styles from "./button.module.css";
 
-const Button = ({ link, children }) => {
+const Button = ({ link, children, onClick }) => {
+  if (link) {
+    return (
+      <Link href={link}>
+        <a className={styles.btn}>{children}</a>
+      </Link>
+    );
+  }
+
   return (
-    <Link href={link}>
-      <a className={styles.btn}>{children}</a>
-    </Link>
+    <button className={styles.btn} onClick={onClick}>
+      {children}
+    </button>
   );
 };
 
--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,13 +1,15 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
 const AllEvents = () => {
   const events = getAllEvents();
 
   return (
-    <div>
+    <>
+      <EventsSearch />
       <EventList items={events} />
-    </div>
+    </>
   );
 };
 export default AllEvents;
