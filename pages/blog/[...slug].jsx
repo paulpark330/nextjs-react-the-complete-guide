534d03ce88faf31a3170e3726b35ee2016565755
Paul Park <paulpark330@hotmail.com>
2022-08-03 23:00:43 -0700

    added catch-all routes

---
+++ pages/blog/[...slug].jsx
@@ -0,0 +1,15 @@
+import { useRouter } from "next/router";
+
+const BlogPostsPage = () => {
+  const router = useRouter();
+
+  console.log(router.query);
+
+  return (
+    <div>
+      <h1>The Blog Posts Page</h1>
+    </div>
+  );
+};
+
+export default BlogPostsPage;
