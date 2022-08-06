1f1d0e553bd49722929962c4d05685f9b2eaba53
Paul Park <paulpark330@hotmail.com>
2022-08-06 15:25:02 -0700

    added revalidate

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,6 +14,7 @@ const HomePage = (props) => {
 };
 
 export async function getStaticProps() {
+  console.log('(Re-)Generating...')
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
@@ -22,6 +23,7 @@ export async function getStaticProps() {
     props: {
       products: data.products,
     },
+    revalidate: 10,
   };
 }
 
