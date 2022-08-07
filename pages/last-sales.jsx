7b58bcc417d9404d5f6aaf6c07b77c5e9453317c
Paul Park <paulpark330@hotmail.com>
2022-08-06 18:27:23 -0700

    combined pre-fetching with client-side fetching

---
+++ pages/last-sales.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const LastSalesPage = (props) => {
+  const [sales, setSales] = useState(props.sales);
+
+  const { data, error } = useSWR(
+    "https://nextjs-course-8fc7f-default-rtdb.firebaseio.com/sales.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
+
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch("https://nextjs-course-8fc7f-default-rtdb.firebaseio.com/sales.json")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         const transformedSales = [];
+
+  //         for (const key in data) {
+  //           transformedSales.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+
+  //         setSales(transformedSales);
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
+
+  if (error) {
+    return <div>Error!</div>;
+  }
+
+  if (!data && !sales) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextjs-course-8fc7f-default-rtdb.firebaseio.com/sales.json"
+  );
+  const data = await response.json();
+
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+
+  return {
+    props: { sales: transformedSales },
+  };
+}
+
+export default LastSalesPage;
