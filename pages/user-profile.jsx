869a36135bbf6d3a54a7fcbd8774ecc52164e579
Paul Park <paulpark330@hotmail.com>
2022-08-06 17:37:02 -0700

    getserversideprops context

--- pages/user-profile.jsx
+++ pages/user-profile.jsx
@@ -5,6 +5,11 @@ const UserProfilePage = (props) => {
 };
 
 export async function getServerSideProps(context) {
+  const { params, req, res } = context;
+
+  console.log(req);
+  console.log(res);
+
   return {
     props: {
       username: "Paul",
